8451fc508c9179a0977431070dd45dc2cddc21ce
unknown <binu.k@binuk7.subexworld.org>
2014-11-07 19:00:18 +0530

    Modifications done for SUC; Facility to add node properties

---
+++ src/features.md
@@ -0,0 +1,22 @@
+Features
+========================================================
+
+New Functionalities
+-------------------
+  * SNAF1 - Optional community finding
+  * SNAF2 - Support for additional input columns as node properties
+    - __Details:__
+    - Additional parameters exposed for adding node properties
+    - This data set should contain all the names of originaiton and termination nodes
+    - This can have any number of additional columns
+  * Handling duplicate vertex
+
+Enhancements
+------------
+  * Save
+    * Graph with all properties
+    * Node properties
+    * Edge properties
+  * Remove Additional data frame for properties
+  * Sort and display enhancements
+
---
+++ src/suc-transform.R
@@ -0,0 +1,72 @@
+library(data.table)
+
+stringsAsFactors=FALSE
+
+# Load data
+loadData = function(dataFile, header = TRUE, sep = ',') {
+  data.frame(read.csv(file=dataFile, header=header, sep=",", stringsAsFactors=FALSE));
+}
+
+
+# Transform into from,to rows
+
+# save to file / invoke network analytics
+
+
+## ----------------- For Subex User Conf ----------------------------------
+loadData = function(dataFile, header = TRUE, sep = ',') {
+  data.frame(read.csv(file=dataFile, header=header, sep=",", stringsAsFactors=FALSE));
+}
+
+prepareData = function (rawData, subex=FALSE)
+{  
+  to = character();
+  from = character();
+  comp = character();
+  attendee = list();  
+  
+
+  for(i in 1:nrow(rawData)) {
+    
+    for (col in names(rawData)) {
+      #       if(subex == TRUE && 
+      #            ((rawData[i, "Company"] == 'Subex') ||
+      #             (col == 'Subex.Limited')))
+      #         continue;
+      
+      
+      
+      if(col == "Name") {
+        # print(paste0("Processing for ", rawData[i, "Name"]));
+        attendee[[tolower(trim(rawData[i, "Name"]))]] = c(lrtrim(rawData[i, "Name"]), rawData[i, "Company"]) ;
+      }
+      else if (col == "Company") {        
+      }      
+      else {      
+        
+        values = unlist(strsplit(as.character(rawData[[col]][i]), "[,]"));        
+        
+        for(val in values) {
+          if(!is.na(val)) {          
+            to = append(to, tolower(trim(val)));
+            from = append(from, tolower(trim(rawData[i, "Name"])));
+            
+            attendee[[tolower(trim(val))]] = c(lrtrim(val), col);
+          }
+        }       
+      }        
+    }    
+  }
+  
+  df = data.frame(from, to, stringsAsFactors=FALSE);
+  
+  # print(head((names)));  
+  write.csv(attendee, 'D:/WorkSpace/R/SNA/data/all-names.csv');
+  write.csv(df, 'D:/WorkSpace/R/SNA/data/suc-conn.csv');  
+}
+  
+lrtrim <- function (x) gsub("^\\s+|\\s+$", "", x)
+trim <- function (x) gsub("\\s+", "", x)
+
+## ----------------- For Subex User Conf ----------------------------------
+
